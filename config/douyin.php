fe027cd8e2574da093f1f4f447927c887e3dbb89
codesinging <codesinging@gmail.com>
2022-01-18 10:46:32 +0800

    add douyin site app configuration

---
+++ config/douyin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+
+return [
+    'clientKey' => env('DOUYIN_SITE_CLIENT_KEY'),
+    'clientSecret' => env('DOUYIN_SITE_CLIENT_SECRET'),
+];
